0aabee93f7d84a44418654d4f98a07122598f178
Cristelle <cristelleal@gmail.com>
2024-10-23 11:28:56 +0200

    feat: create html files and update python converting script

--- script/script.py
+++ script/script.py
@@ -23,9 +23,18 @@ def convert_md_to_html(input_file, output_file):
     except FileNotFoundError:
         print(f"Le fichier {input_file} n'a pas été trouvé.")
 
-if __name__ == "__main__":
-    # Exemple d'utilisation
-    input_md = "./atelier1/2025-01-18-evenement-1.md"
-    output_html = "./view/index.html"
-
+def process_event(input_md, output_html):
     convert_md_to_html(input_md, output_html)
+
+if __name__ == "__main__":
+    events = [
+        ("./atelier1/2025-01-18-evenement-1.md", "./view/neighbor-party.html"),
+        ("./atelier1/2025-02-16-evenement-2.md", "./view/gardening-workshop.html"),
+        ("./atelier1/2025-03-23-evenement-3.md", "./view/lilas-run.html"),
+        ("./atelier1/2025-04-11-evenement-4.md", "./view/book-festival.html"),
+        ("./atelier1/2025-05-17-evenement-5.md", "./view/eco-day.html"),
+        ("./atelier1/2025-06-21-evenement-6.md", "./view/party-lilas-music.html")
+    ]
+
+    for input_md, output_html in events:
+        process_event(input_md, output_html)
